2ef7caae1c3ac1d2633232e476cd3f0a8a9eeac8
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-08-08 22:12:56 -0700

    loginTest

---
+++ ApplicationDB/LoginHandler.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+const sqlite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+
+const db_path = "res://db_files/db_main"
+
+var db : sqlite = sqlite.new()
+
+
+
+func _on_ButtonLogin_pressed() -> void:
+	
+	db.path = db_path
+	db.open_db()
+	
+	var _user_string = $TextUsername.text
+	var _pass_string = $TextPassword.text
+	
+	print(db.query("SELECT Password FROM Credentials WHERE Username = '" + _user_string + "';"))
+	
+	if db.query_result.size() == 1:
+		if _pass_string == db.query_result[0]["Password"]:
+			print("Login successful as " + _user_string)
+		
+	elif db.query_result.size() == 0:
+		print("No username matching")
+	
+	
