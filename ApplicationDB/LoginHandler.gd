80d841c7e66dce8ea3f0824000cde715a8c24c20
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-08-11 10:52:33 -0700

    enter to login

--- ApplicationDB/LoginHandler.gd
+++ ApplicationDB/LoginHandler.gd
@@ -10,9 +10,18 @@ var db : sqlite = sqlite.new()
 
 var logged_in_as = null
 
+onready var _pass = $Entry/HBoxContainer/TextPassword
+onready var _user = $Entry/HBoxContainer/TextUsername
+
 func _ready() -> void:
 	db.path = db_path
 	createEvents()
+
+func _process(delta: float) -> void:
+	if get_focus_owner() == _pass or get_focus_owner() == _user:
+		if Input.is_action_just_pressed("ui_accept"):
+			_on_ButtonLogin_pressed()
+
 
 func createEvents() -> void:
 	db.open_db()
@@ -27,48 +36,48 @@ func createEvents() -> void:
 	db.close_db()
 
 func _on_ButtonLogin_pressed() -> void:
-	
-	db.path = db_path
 	db.open_db()
 	
-	var _user_string = $TextUsername.text
-	var _pass_string = $TextPassword.text
+	var _user_string = _user.text
+	var _pass_string = _pass.text
 	
-	print(db.query("SELECT Password FROM Credentials WHERE Username = '" + _user_string + "';"))
+	db.query("SELECT Password FROM Credentials WHERE Username = '" + _user_string + "';")
+	print(db.query_result)
 	
 	if db.query_result.size() == 1:
 		if _pass_string == db.query_result[0]["Password"]:
 			print("Login successful as " + _user_string)	
-			$OutputConsole.show()
-			$OutputConsole.text="Login success."
-			logged_in_as = _user_string
-			
-			$TextPassword.hide()
-			$TextUsername.hide()
-			$ButtonLogin.hide()
-			$HoursWorked.show()
+			#$OutputConsole.show()
+			#$OutputConsole.text="Login success."
+			_login_success(_user_string)
 			
 		else:
 			print("Password Incorrect")
-			$OutputConsole.show()
-			$OutputConsole.text="Password Incorrect"
+			#$OutputConsole.show()
+			#$OutputConsole.text="Password Incorrect"
 	elif db.query_result.size() == 0:
 		print("No username matching")
-		$OutputConsole.show()
-		$OutputConsole.text="No Username Matching"
+		#$OutputConsole.show()
+		#$OutputConsole.text="No Username Matching"
 		
 	db.close_db()
 
 
+func _login_success(username) -> void:
+	logged_in_as = username
+	$Entry.hide()
+	$Members.show()
+
+
 func _on_HoursWorked_pressed() -> void:
-	$OutputConsole.text = "You've worked "
+	#$OutputConsole.text = "You've worked "
 	
 	db.open_db()
 	db.query("SELECT Hours FROM MemberData WHERE Username = '" + logged_in_as + "';")
 	
 	var hours = db.query_result[0]["Hours"]
 	
-	$OutputConsole.text += str(hours) + " hours."
+	#$OutputConsole.text += str(hours) + " hours."
 	
 	db.close_db()
 
