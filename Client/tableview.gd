5abe6d9ed6f5a0f1396254a19d50165cfbd3fd76
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-08-22 12:08:15 -0700

    Invoice form fills in with some shit

--- Client/tableview.gd
+++ Client/tableview.gd
@@ -10,16 +10,45 @@ func _visibility_changed() -> void:
 
 
 remote func _initialize_records(records):
-	for child in $ScrollContainer/VBoxContainer.get_children():
-		child.free()
+	for i in $ScrollContainer/VBoxContainer.get_child_count():
+		if i > 0:
+			$ScrollContainer/VBoxContainer.get_child(i).queue_free()
 	
+	
+	var totalplayercontri = 0
+	var playernet = 0
+	var orgnet = 0
+	var overalltotalgross = 0
 	for record in records:
 		var inst = preload("res://PayslipRecord.tscn").instance()
 		
 		$ScrollContainer/VBoxContainer.add_child(inst)
 		
+		$VBoxContainer/Username.text = record["Username"]
+		
 		inst.set_player_gross(record["Grossed"])
+		totalplayercontri += record["Grossed"]
 		inst.set_player_net(record["PlayerNet"])
+		playernet += record["PlayerNet"]
 		inst.set_total_gross(record["TotalGrossed"])
+		overalltotalgross += record["TotalGrossed"]
 		inst.set_event_name(record["EventName"])
-		inst.set_fee("FEE VIEW TO DO")
+		
+		inst.set_fee(record["TotalGrossed"] * 0.005) # lol
+		
+		orgnet += record["OrgNet"]
+		
+	
+	var vbox = $HBoxContainer/VBoxContainer2
+	
+	var grosslabel = vbox.get_node("TotalPlayerGross").text
+	vbox.get_node("TotalPlayerGross").text = grosslabel % str(totalplayercontri)
+	
+	var orgnetlabel = vbox.get_node("OrgNet").text
+	vbox.get_node("OrgNet").text = orgnetlabel % str(orgnet)
+	
+	var grandtotallabel = vbox.get_node("GrandTotal").text
+	vbox.get_node("GrandTotal").text = grandtotallabel % str(overalltotalgross)
+	
+	var playernetlabel = vbox.get_node("PlayerNet").text
+	vbox.get_node("PlayerNet").text = playernetlabel % str(playernet)
