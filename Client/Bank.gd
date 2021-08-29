79efa93a07f215ebf1928ceac9715a2ead1f3feb
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-08-29 14:04:48 -0700

    spaces gotten rid of in bank nodes

--- Client/Bank.gd
+++ Client/Bank.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _ready() -> void:
 	connect("visibility_changed", self, "_visibility_changed")
+	
 
 
 func _visibility_changed() -> void:
---
+++ Client/TabContainer.gd
@@ -0,0 +1,5 @@
+extends TabContainer
+
+func _ready() -> void:
+	set_tab_title(0, "Custom Transactions")
+	set_tab_title(1, "Paid To Members")
